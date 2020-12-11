8240dfefc0b07666500c3fb64e97086d1a66b66f
Mark Harfouche <mark.harfouche@gmail.com>
2020-12-10 23:09:41 -0500

    Add option to diable hugin gui

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,5 +1,8 @@
 set -ex
 
+cp $RECIPE_DIR/CMakeLists.txt .
+cp $RECIPE_DIR/hugin1CMakeLists.txt src/hugin1/CMakeLists.txt
+
 mkdir build
 cd build
 
@@ -7,6 +10,7 @@ cmake -LAH                              \
     -DCMAKE_BUILD_TYPE="Release"        \
     -DCMAKE_PREFIX_PATH=${PREFIX}       \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}    \
+    -DENABLE_GUI=OFF                    \
     ..
 
 make -j${CPU_COUNT}
